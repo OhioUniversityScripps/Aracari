9238a80288f50c67c463653cbf5b6d2d2db44890
Ricky Chilcott <ricky@rbsavvy.com>
2013-03-02 10:32:35 -0500

    Generating rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aracari::Application.config.secret_token = 'c3d5eb2e3d6b8005ef3c771537d8d7fb5f1b5726730e01b87ec9e5711dbe03adcd4fb1e963b752fb68b1c4dae5cc97954db6c5a8d9675d749be2a9c0504b01e0'
