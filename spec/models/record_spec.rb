59955eda67baa8964855794b02713706b8862231
Ricky Chilcott <ricky@rbsavvy.com>
2013-04-13 16:16:40 -0400

    Set dynamic_allow_attribute is true

--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -7,4 +7,14 @@ describe Record do
     record.should be_valid
   end
   
+  it "should support dynamic fields" do
+    expect { record.pest }.to raise_error NoMethodError
+    
+    record[:pest] = true
+    record[:pest].should be_true
+    
+    record.write_attribute(:speed, "33 RPM").should be_true
+    record.read_attribute(:speed).should be_true
+  end
+  
 end
