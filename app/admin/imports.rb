6983014d99e2cda91f7c5064db4ff9c0174d20cb
Ricky Chilcott <ricky@rbsavvy.com>
2013-04-13 18:39:12 -0400

    Stubbing out imports resource for activeadmin

--- app/admin/imports.rb
+++ app/admin/imports.rb
@@ -1,3 +1,17 @@
 ActiveAdmin.register Import do
 
+  index do
+    column :uploader_url
+    column :created_at
+    
+    default_actions
+  end
+  
+  form do |f|
+    f.inputs "Content" do
+      f.input :uploader, as: :file
+    end
+    
+    f.actions
+  end
 end
