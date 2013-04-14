00d67b1f677e8591bf498c00de46e3a5c30e8fb2
Ricky Chilcott <ricky@rbsavvy.com>
2013-04-14 00:49:48 -0400

    Setting local storage

--- app/models/import.rb
+++ app/models/import.rb
@@ -1,5 +1,16 @@
+# require 'zip/zipfilesystem'
+
 class Import < ActiveRecord::Base
+  
   attr_accessible :archive
   
-  has_attached_file :archive
+  has_attached_file :archive, :storage => :filesystem
+  
+  # def after_create
+  #   Zip::ZipFile.open(self.archive_path) do |zipfile|
+  #     zipfile.each do |file|
+  #       # do something with file
+  #     end
+  #   end
+  # end
 end
