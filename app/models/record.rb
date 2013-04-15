aa216e1b61cc91c2ca5d7de5136cf3d781389582
Ricky Chilcott <ricky@rbsavvy.com>
2013-04-15 18:08:06 -0400

    Adding basic parse functionality

--- app/models/record.rb
+++ app/models/record.rb
@@ -2,5 +2,31 @@ class Record < ActiveRecord::Base
   attr_accessible :legacy_asset_id,  :title,  :creators,  :category,  :content_description,  :further_review,  :assessors,  :collection_id,  :shelf_code,  :year,  :month,  :day,  :generation,  :unique_id,  :size,  :media_type,  :speed,  :stock_manufacturer,  :track_configuration,  :stock_brand,  :tape_thickness,  :stock_formula,  :tape_playback_direction,  :asset_age,  :tape_width,  :known_running_time,  :tape_noise_reduction,  :popped_strands,  :stepped_pack,  :flange_pack,  :unsecured_tape,  :heads_out,  :vinegar_odor,  :other_odor,  :backing_loss,  :binder_loss,  :blocking,  :brittleness,  :breakage,  :cinching,  :cupping,  :delamination,  :edge_curling,  :edge_damage,  :stretching,  :spoking,  :windowing,  :wrinkling,  :dirt_dust,  :fungus,  :liquid,  :pest,  :slotted_hub,  :splices,  :record_cassette_cracks,  :record_cassette_delamination,  :record_cassette_scratches,  :record_cassette_surface_contamination,  :record_cassette_warping,  :condition_notes,  :notes
 
   # embeds_many :images, cascade_callbacks: true
-  # # has_many :images
+  # has_many :images
+  
+  def self.parse(xml)
+    doc = Nokogiri.XML(xml)
+    
+    # loop through each record
+    doc.xpath('//record').each do |record|
+      # build a new record
+      record_builder = Record.new
+            
+      record.xpath('///field').each do |field|
+        # set all attributes        
+        case
+          when field  == 'Unnamed Field' # all images
+          
+          else # default
+            attribute, value = field[:name].parameterize.underscore.to_sym, field.text.strip
+            
+            record_builder[attribute] = value
+        end
+      end
+      
+      # save
+      record_builder.save!
+      # binding.pry
+    end
+  end
 end
--- spec/models/record_spec.rb
+++ spec/models/record_spec.rb
@@ -10,5 +10,30 @@ describe Record do
   it "should not support dynamic fields" do
     expect { record.new_attribute }.to raise_error NoMethodError
   end
-  
+
+  describe "#parse" do
+    let(:data) { File.read(Rails.root + "spec/support/record_parse/example.xml") }
+    
+    before(:each) do
+      Record.destroy_all
+    end
+    
+    it "should parse a valid example record" do
+      Record.parse(data)
+      
+      Record.count.should be(1)
+      Record.first.legacy_asset_id.should eq("Music Tape 767 Pt.E V.6")
+      Record.first.year.should eq(1978)
+    end
+    
+    pending "should import images correctly" do
+      Record.parse(data)
+      
+      Record.first.images.count.should be(4)
+    end
+    
+    pending "should not error out on bad example" do
+      
+    end
+  end
 end
