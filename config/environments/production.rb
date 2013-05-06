b0b1bc864efffad50be52a4d1633581f98f74d7c
Ricky Chilcott <ricky@rbsavvy.com>
2013-05-06 06:58:51 -0400

    Disregard mail errors in production (for now)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,5 +65,6 @@ Aracari::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
   
-  config.action_mailer.default_url_options = { :host => 'guyana1804grant.dnsd.info:3000' }
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.raise_delivery_errors = false
 end
